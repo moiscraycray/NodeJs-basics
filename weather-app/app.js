d7cb3a4828dcbc2543e1c99d0a44bde3049d1a25
Olivia Mo <oliviatrademark@gmail.com>
2017-12-25 11:25:59 +1100

    added encoder in app.js so http is not hardcoded with address

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to fetch weather for',
+      string: true // this tells yargs to always parse the address as string
+    }
+  })
+  .help()
+  .alias('help', 'h') // takes 2 arguments: The actual argument that you want to set an alias for, and the alias
+  .argv; // this takes all the above configuration runs through our arguments and stores the result in the argv variable on line 4
+
+console.log(argv);
+
+// 170 avoca street
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=170%20avoca%20street%20sydney',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`, // we're encoding the user input (from string to %20)
   json: true // this tells request that the data coming back is going to be json data and that it should go ahead take that json string and convert it to an object for us
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`); // The results[0].formatted_address is from the json view in your chrome browser. It is the blue box that appears when you hover over a piece of data.
