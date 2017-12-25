0a25e2ea9d5c557459ed58f8501bdaaeb01d9d0b
Olivia Mo <oliviatrademark@gmail.com>
2017-12-25 16:57:58 +1100

    refactored weather app into its own separate file and take dynamic data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,44 +1,37 @@
-// const yargs = require('yargs');
-//
-// // we're requiring a file in another directory
-// const geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'address to fetch weather for',
-//       string: true // this tells yargs to always parse the address as string
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h') // takes 2 arguments: The actual argument that you want to set an alias for, and the alias
-//   .argv; // this takes all the above configuration runs through our arguments and stores the result in the argv variable on line 7
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2)); // JSON stringify converts json object to human-readable string. 2nd argument is used to filter out properties but we're not doing anything with it so it's set to undefined. 3rd argument  specifies how many spaces you want in your indentation
-//   }
-// });
-// // Here we're calling the function geocodeAddress from the geocode file defined on line 4. We're passing the user input 'address' tot he geocodeAddress function
-// // We're also doing a callback function. This function will get called after the request comes back.
-//
-// // c37ef8772392edd1fafcc111bf91c2bc
+const yargs = require('yargs');
 
-const request = require('request');
+// we're requiring a file in another directory
+const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 
-request({
-  url:`https://api.darksky.net/forecast/c37ef8772392edd1fafcc111bf91c2bc/-33.894444,150.9375`,
-  json: true
-}, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    let fah = body.currently.temperature;
-    let cel = (fah - 32) * 0.5556;
-    console.log(`Temperature in Cabramatta: ${Math.floor(cel)}C`);
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to fetch weather for',
+      string: true // this tells yargs to always parse the address as string
+    }
+  })
+  .help()
+  .alias('help', 'h') // takes 2 arguments: The actual argument that you want to set an alias for, and the alias
+  .argv; // this takes all the above configuration runs through our arguments and stores the result in the argv variable on line 7
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
   } else {
-    console.log('Unable to fetch weather');
+    console.log(results.address)
+    // console.log(JSON.stringify(results, undefined, 2)); // JSON stringify converts json object to human-readable string. 2nd argument is used to filter out properties but we're not doing anything with it so it's set to undefined. 3rd argument  specifies how many spaces you want in your indentation
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) { // results.lat/long from geocode.js line 16 & 17
+        console.log(errorMessage);
+      } else {
+        // console.log(JSON.stringify(weatherResults, undefined, 2));
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`) // from weather.js line 14 & 15
+      }
+    });
   }
 });
+// Here we're calling the function geocodeAddress from the geocode file defined on line 4. We're passing the user input 'address' tot he geocodeAddress function
+// We're also doing a callback function. This function will get called after the request comes back.
---
+++ weather-app/weather/weather.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+  request({
+    url:`https://api.darksky.net/forecast/c37ef8772392edd1fafcc111bf91c2bc/${lat},${lng}`,
+    json: true
+  }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let fah = body.currently.temperature;
+      let cel = (fah - 32) * 0.5556;
+      let appFah = body.currently.apparentTemperature;
+      let appCel = (appFah - 32) * 0.5556;
+      callback(undefined, {
+        temperature: Math.floor(cel),
+        apparentTemperature: Math.floor(appCel)
+      })
+    } else {
+      callback('Unable to fetch weather');
+    }
+  });
+}
+
+module.exports = {
+  getWeather
+}
+// same as module.exports.getWeather = getWeather;
