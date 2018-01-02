94e9d7a83893e3add3671a9d31e9628cf2687f2c
Olivia Mo <oliviatrademark@gmail.com>
2018-01-02 15:30:46 +1100

    added partials and helpers

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,8 @@ const hbs = require('hbs');
 
 var app = express();
 
-app.set('view engine', 'hbs') // this lets us set some various express related configurations. We're passing a key value pair, the key is the thing you want to set and the value is the value you want to use; so here we're setting the 'view engine' to 'hbs'
+hbs.registerPartials(__dirname + '/views/partials'); // This is going to take the directory you want to use for all your handlebar partial files
+app.set('view engine', 'hbs'); // this lets us set some various express related configurations. We're passing a key value pair, the key is the thing you want to set and the value is the value you want to use; so here we're setting the 'view engine' to 'hbs'
 app.use(express.static(__dirname + '/public')); // to use middleware, add this line. Middleware lets you make tweaks to how express works. Static() takes the absolute path to the folder you want to serve up (the complete path) so we use __dirname - it's a variable that stores the path to your projects directory ie node-web-server. We're concatenating the 'public' folder to the end of the file path to access the folder 'public'
 
 // app.get('/', (req, res) => { //sets up handler for HTTP get request. 1st arg: url (e.g.root of app) 2nd arg: what to send back to the person who made the request (req, res). (Req)uest stores info on things coming in like headers that were used, body information, methods that were made with the request to the path. (Res)ponse has a lot of methods available, so you can respond to the HTTP request in whatever way you like.
@@ -17,19 +18,28 @@ app.use(express.static(__dirname + '/public')); // to use middleware, add this l
 //   })
 // });
 
+hbs.registerHelper('getCurrentYear', () => { // 'getCurrentYear' will be called inside partials
+  return new Date().getFullYear();
+}); // 2 arguments: 1st arg: name of the helper. 2nd arg: function to run
+// we're using a helper here because we don't want to write currentYear new Date().getFullYear() everytime in the app.get methods. Instead, we are calling this helper method directly from the partial
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome friend!'
+    // currentYear new Date().getFullYear()
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', { // render lets you render any of the templates you have set up with your current view engine. The default directory is 'views' for express, our about.hbs in that folder is the template
-    pageTitle: 'About page',
-    currentYear: new Date().getFullYear() // The 2nd argument of render is an object. We pass the keys (pageTitle, currentYear) to the about.hbs to create a dynamic page with dynamic data. To use these, in our about.hbs we need to use handlebars syntax {{}}
-  });
+    pageTitle: 'About page'
+    // currentYear new Date().getFullYear()
+  }); // The 2nd argument of render is an object. We pass the keys (pageTitle, currentYear) to the about.hbs to create a dynamic page with dynamic data. To use these, in our about.hbs we need to use handlebars syntax {{}}
 });
 
 app.get('/bad', (req, res) => {
