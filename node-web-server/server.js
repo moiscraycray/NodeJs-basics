15a2236f778dc14bd8c4f80464e7b97ee18ee7e1
Olivia Mo <oliviatrademark@gmail.com>
2018-01-02 13:32:30 +1100

    installed and used handlebars to create some static pages

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,35 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs') // this lets us set some various express related configurations. We're passing a key value pair, the key is the thing you want to set and the value is the value you want to use; so here we're setting the 'view engine' to 'hbs'
 app.use(express.static(__dirname + '/public')); // to use middleware, add this line. Middleware lets you make tweaks to how express works. Static() takes the absolute path to the folder you want to serve up (the complete path) so we use __dirname - it's a variable that stores the path to your projects directory ie node-web-server. We're concatenating the 'public' folder to the end of the file path to access the folder 'public'
 
-app.get('/', (req, res) => { //sets up handler for HTTP get request. 1st arg: url (e.g.root of app) 2nd arg: what to send back to the person who made the request (req, res). (Req)uest stores info on things coming in like headers that were used, body information, methods that were made with the request to the path. (Res)ponse has a lot of methods available, so you can respond to the HTTP request in whatever way you like.
-  //res.send('<h1>Hello Express!</h1>') // This lets us respond to the request, sending some data back.
-  res.send({ // this sends json data
-    name: 'Olivia',
-    likes: ['Christina',
-    'cats',
-    'dogs',
-    'food']
+// app.get('/', (req, res) => { //sets up handler for HTTP get request. 1st arg: url (e.g.root of app) 2nd arg: what to send back to the person who made the request (req, res). (Req)uest stores info on things coming in like headers that were used, body information, methods that were made with the request to the path. (Res)ponse has a lot of methods available, so you can respond to the HTTP request in whatever way you like.
+//   //res.send('<h1>Hello Express!</h1>') // This lets us respond to the request, sending some data back.
+//   res.send({ // this sends json data
+//     name: 'Olivia',
+//     likes: ['Christina',
+//     'cats',
+//     'dogs',
+//     'food']
+//   })
+// });
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome friend!'
   })
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', { // render lets you render any of the templates you have set up with your current view engine. The default directory is 'views' for express, our about.hbs in that folder is the template
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear() // The 2nd argument of render is an object. We pass the keys (pageTitle, currentYear) to the about.hbs to create a dynamic page with dynamic data. To use these, in our about.hbs we need to use handlebars syntax {{}}
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -26,5 +39,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000'); 
+  console.log('Server is up on port 3000');
 }); // app.listen is going to bind the application to a port on our machine e.g. 3000
