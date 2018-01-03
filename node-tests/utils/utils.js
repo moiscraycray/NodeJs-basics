579e7cf0afd62bbd40e5eae60cbf130935b03a2a
Olivia Mo <oliviatrademark@gmail.com>
2018-01-03 13:09:43 +1100

    added simple tests to testing playground

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -3,3 +3,11 @@ module.exports.add = (a, b) => {
 };
 
 module.exports.square = (x) => x * x;
+
+module.exports.setName = (user, fullName) => {
+  var names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  // console.log(user)
+  return user;
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,52 @@
+const expect = require('expect');
 const utils = require('./utils.js')
 
 it('should add two numbers', () => {
   let result = utils.add(33, 11);
-  if (result !== 44) {
-    throw new Error(`Expected 44, but got ${result}.`);
-  }
+  expect(result).toBe(44).toBeA('number'); // we can chain on anotehr call
 });
 
 it('should square a number', () => {
-  let result = utils.square(2);
-  if (result !== 4) {
-    throw new Error(`Expected 4, but got ${result}.`);
+  let result = utils.square(3);
+  expect(result).toBe(9).toBeA('number'); // this works with strings too
+});
+
+it('should expect the same number', () => {
+  expect(12).toBe(12);
+});
+
+it('should expect some values', () => {
+  expect({name: 'Olivia'}).toEqual({name: 'Olivia'});
+});
+// This checks whether the object properties are the same. Will not work with toBe. We need to use toEqual for objects and arrays
+
+it('should expect something different', () => {
+  expect(['Olivia']).toNotEqual(['olivia']);
+})
+
+it('should have some values', () => {
+  expect([2,3,4]).toInclude(2);
+});
+// toInclude checks if an array/object includes some things.
+
+it('should have some different key values', () => {
+  expect({
+    name: 'Olivia',
+    age: 23,
+    location: 'Australia'
+  }).toExclude({
+    age: 25
+  })
+});
+
+it('should set firstName and lastName', () => {
+  let user = {
+    location: 'Australia',
+    age: 23
   }
+  let res = utils.setName(user, 'Olivia Mo')
+  expect(res).toInclude({
+    firstName: 'Olivia',
+    lastName: 'Mo'
+  }); // user object includes firstName and lastName from the util
 });
