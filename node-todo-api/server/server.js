fca8254912eee5911f3d941b1915830bbf8b8739
Olivia Mo <oliviatrademark@gmail.com>
2018-01-07 21:38:50 +1100

    add POST /todos route and refactor mongoose

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,69 +1,30 @@
-var mongoose = require('mongoose');
+// install express & Body-parser in terminal
+// npm i express@4.14.0 body-parser@1.15.2 --save
+// body-parse let us send JSON to the server. The server can then take that JSON and do something with it. Body-parser essentiall parses the body. It takes that string body and turns it into a javascript object.
+var express = require('express');
+var bodyParser = require('body-parser');
 
-// line 6 connects to database
-// The difference between MongoClient and Mongoose is that the MongoClient connect method takes a callback and that is when we have access to the database. Mongoose is a lot more complex. This is good because it means our code can be a lot simpler. Mongoose is maintaining this connection over time. We don’t have to micromanage the order things happen in, mongoose takes care of that for us.
-mongoose.Promise = global.Promise; // only need to set line5&6 once, no need to put it in any other file. line 5 lets us use promise in mongoose
-mongoose.connect('mongodb://localhost: 27017/TodoApp');
+var {mongoose} = require('./db/mongoose.js'); //destructing object
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// Here we're creating a mongoose.model so mongoose knows how to store our data.
-// validators: http://mongoosejs.com/docs/validation.html
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String, // if user enters number or boolean, it'll still work; mongoose will wrap them in quotes
-    required: true, //this and below are validators, making sure we have valid data
-    minlength: 1,
-    trim: 1 // trims any white space before and after first&last character
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json()); // this sends json to our express application. All it does is parse the body of a POST request as JSON, if the 'content type' of the request is 'application/json'
+
+app.post('/todos', (req, res) => { // 2 args: 1st URL, 2nd callback. We use forward-slash is convention for resource creation (creating a new todo)
+  // console.log(req.body); // body is stored by bodyParser on line 13
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc); // sends the todo info back to user like ID and completed status
+  }, (error) => {
+    res.status(400).send(error); // .status(400) is optional but we want to let the user know what happened to their get request (400 is bad request)
+  });
 });
 
-var newTodo = new Todo({ // we're creating a new instance of the Todo model from line 9
-  text: 'Cook dinner'
-});
-// Creating an instance will not update the database, we need to use .save which returns a promise
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc); //success promise (ie resolve)
-}, (error) => {
-  console.log('Unable to save todo'); // failed promise (ie reject)
-});
-
-// new todo instance
-let anotherTodo = new Todo({
-  text: 'Send a cute message to babe',
-  completed: true,
-  completedAt: 1900
-});
-
-anotherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (error) => {
-  console.log('Unable to save todo');
-});
-
-// User
-// email -require, trim, type, minlength 1
-var UserEmail = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-// creating new instance of UserEmail
-var newUser = new UserEmail({
-  email: '     olivia@example.com.au    '
-})
-
-newUser.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (error) => {
-  console.log('Unable to save new user');
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
