c50dacc901f3494dc29ba34e103f6f1ee2b7e707
Olivia Mo <oliviatrademark@gmail.com>
2018-01-09 16:40:08 +1100

    Add tests for PATCH /todos/:id

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 // this is going to let us run some code before every single test case. We're going to use beforeEach to set up the database in a way that's useful. For now, we're going to make sure the database is empty because in the test, we assume there'll be only 1 item (but in reality, there'll be more than one todo item)
@@ -149,4 +151,46 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    let hexId = todos[0]._id.toHexString();
+    let text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text // text: text is ES5 sytanx
+      })
+      .expect(200)
+      .expect((res) => { // custom expect call to test the body that comes back matches the body at line 159
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    let hexId = todos[1]._id.toHexString();
+    let text = 'This should be the new text!!';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text // text: text is ES5 sytanx
+      })
+      .expect(200)
+      .expect((res) => { // custom expect call to test the body that comes back matches the body at line 159
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+
+  // we didnt add verification of IDs, you can add them but here, we've done it so many times
 });
